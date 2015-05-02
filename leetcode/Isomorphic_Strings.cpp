09247e41c6c1f615a075bbcea357f788cd8ceaf5
John Shaw <bard2093@gmail.com>
2015-05-02 21:52:53 +0800

    Isomorphic String

--- leetcode/Isomorphic_Strings.cpp
+++ leetcode/Isomorphic_Strings.cpp
@@ -8,21 +8,37 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
+
+//hashmap
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        for (int i = 0; i < s.length(); i++) {
-            char c1 = s.at(i);
-            cout<<c1;
+        map<char, char> m1;
+        map<char, char> m2;
+        for(int i=0; i<s.size(); ++i)
+        {
+            if(m1.find(s[i])==m1.end() && m2.find(t[i])==m2.end())
+            {
+                m1[s[i]] = t[i];
+                m2[t[i]] = s[i];
+            }
+            else if(m1.find(s[i])!=m1.end() && m2.find(t[i])!=m2.end())
+            {
+                if(m1[s[i]]!=t[i] || m2[t[i]]!=s[i])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
-        
-        
         return true;
     }
-private:
-    int hash[26];
 };
 
