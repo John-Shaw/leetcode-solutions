bf6f40e38f9fcf3867377769281b0f9c70f7fdcb
John Shaw <bard2093@gmail.com>
2015-05-29 22:07:07 +0800

    Maximum Depth of Binary Tree

---
+++ leetcode/Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,30 @@
+//
+//  Maximum_Depth_of_Binary_Tree.cpp
+//  leetcode
+//
+//  Created by 邵建勇 on 15/5/29.
+//  Copyright (c) 2015年 John Shaw. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (0 == root) {
+            return 0;
+        }
+        
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return left>right?left+1:right+1;
+    }
+};
